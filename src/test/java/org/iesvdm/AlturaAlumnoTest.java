2a8ad7b78cc2afc0ecbe8f91076c268d29d599dd
Neklof1982 <neklof1982@hotmail.com>
2024-03-14 13:04:25 +0100

    Assert Upadtes

---
+++ src/test/java/org/iesvdm/AlturaAlumnoTest.java
@@ -0,0 +1,88 @@
+package org.iesvdm;
+// De esta forma te obliga a usar Assertions delante del código
+//import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
+//Esta trayendo todo el código estático de la clase Assertions
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+//Esta trayendo todo el código estatico de la clase Test
+
+public class AlturaAlumnoTest {
+
+    @Test
+    void  verdadero() {
+        // al importar static, no hace falta escribir Assertions
+         assertTrue ( 1==1);
+
+        //Sino importarmos el código estatico es la forma:
+        //Assertions.assertTrue ( 1==1);
+    }
+    @Test
+    void aniadeNombre() {
+
+        final String [] array = new String [10];
+
+        array[0] ="Jose";
+        array[1] ="Paco";
+        String nombre = "Maria";
+        String[] arrayActual = AlturaAlumno.añadeNombre
+                (array, "Maria");
+
+        assertTrue( arrayActual[arrayActual.length-1].equals(nombre));
+        // verifica que el nombre "Maria" esta en la posicion ultima
+    }
+    @Test
+    void aniadeNombre2() {
+        final String [] array = new String[0];
+
+        String nombre ="Maria";
+        String[] arrayActual = AlturaAlumno.añadeNombre(array, nombre);
+
+        assertEquals(nombre, arrayActual[arrayActual.length-1]);
+    }
+
+    @Test
+    void aniadeNombre3() {
+        final String [] array = new String[0];
+        int longInicial = array.length;
+        String nombre ="Maria";
+        String[] arrayActual = AlturaAlumno.añadeNombre(array, nombre);
+
+        assertEquals(longInicial+1, arrayActual.length);
+        // verifica que la longitud de la cadena es la correcta
+    }
+
+    @Test
+    void aniadeNombre4() {
+
+        final String [] array = new String [10];
+
+        array[0] ="Jose";
+        array[1] ="Paco";
+        String nombre = "Maria";
+        String[] arrayActual = AlturaAlumno.añadeNombre
+                (array, "Maria");
+
+        String[] arrayExpected = Arrays.copyOf(array, array.length+1);
+        arrayExpected[arrayExpected.length-1] = nombre;
+        assertArrayEquals(arrayExpected, arrayActual);
+    }
+
+    @Test
+    void aniadeAltura () {
+        final double [] array = new double [10];
+
+        array[0] = 1.70;
+        array[1] = 1.90;
+        double altura = 1.5;
+        //double[] arrayActual = AlturaAlumno.añadeAltura
+        //        (array, 1.5);
+
+        //assertTrue( arrayActual[arrayActual.length-1].equals(altura));
+        // verifica que el nombre "Maria" esta en la posicion ultima
+
+    }
+
+
+}
